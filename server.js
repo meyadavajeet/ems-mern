b39b90242cf9b72fe12c93c9925963a8fa43f4c8
meyadavajeet <meyadavajeet@gmail.com>
2023-01-23 15:05:53 +0530

    server file for production

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use(cors());
 /**
  * configure static pages for the production deployment
  */
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 /**
